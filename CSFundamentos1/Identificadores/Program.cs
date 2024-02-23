65faf420e623b1612d5873632327a4b4246f7eeb
Tiago Ferrari <tiago-projects@outlook.com>
2024-02-23 17:02:50 -0300

    add Identificadores

---
+++ CSFundamentos1/Identificadores/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Definindo Identificadores");
+Console.WriteLine();
+
+// identificadores válidos
+string nome;
+string nomeCompleto;
+int idade;
+int _valor;
+int idade1;
+
+// identificadores inválidos
+//int 5idade;
+//int $valor;
+//int valor#total;
+//string nome Completo;
+
+// para nomes de variáveis: camelCase
+string descontoTotal;
+string desconto_total;
+
+// constantes: maíusculas
+double PI = 3.14;
+string PREFIXO = "16";
+string PREFIXO_SP = "11";
+
+Console.ReadLine();
+
+// para nomes de classes e métodos: PascalCase
+class ImprimirTexto
+{
+    public void ImprimeNome()
+    {
+        Console.WriteLine("Ferrari");
+    }
+}
--- CSFundamentos1/TipoDeDados2/obj/Debug/net6.0/TiposDeDados2.AssemblyInfo.cs
+++ CSFundamentos1/TipoDeDados2/obj/Debug/net6.0/TiposDeDados2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiposDeDados2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd38cba30ca301542f7e6458f1d4b98a6200e85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2d75362f6ce924612520dffcc36256a1d981702")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiposDeDados2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiposDeDados2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
