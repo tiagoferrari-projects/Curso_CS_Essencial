2ac025e2b4ea5029179297a7b140cc970c22d7fc
Tiago Ferrari <tiago-projects@outlook.com>
2024-02-23 00:17:27 -0300

    add TiposDeDados3

---
+++ CSFundamentos1/TiposDeDados3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Atribuindo valores a bool e char");
+
+bool ativo = true;
+Boolean inativo = false;
+
+Console.WriteLine(ativo);
+Console.WriteLine(inativo);
+
+Console.WriteLine(10 == 15);
+Console.WriteLine(10 == 10);
+
+int x = 10;
+int y = 15;
+
+Console.WriteLine(x > y);
+
+char letra1 = 'A';
+char letra2 = '\u0041';
+Console.WriteLine($"Somente uma letra: {letra1}");
+Console.WriteLine($"Código Unicode: {letra2}");
+
+
+Console.ReadLine();
