d2d75362f6ce924612520dffcc36256a1d981702
Tiago Ferrari <tiago-projects@outlook.com>
2024-02-23 16:42:13 -0300

    add NullableTypes1

---
+++ CSFundamentos1/NullableTypes1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("## Nullable Types ##\n");
+Console.WriteLine();
+
+// declaração simplificada
+// int? i = null;
+// double? d = null;
+// bool? b = null;
+
+// declaração com tipo Nullable
+// Nullable<int> i = null;
+// Nullable<double> d = null;
+// Nullable<bool> b = null;
+
+// Console.WriteLine(i);
+// Console.WriteLine(d);
+// Console.WriteLine(b);
+
+// operador de coalescência nula ??
+// int? x = 1;
+// int? x = null;
+// int y = x ?? 0;
+// Console.WriteLine(y);
+
+// int? x = 4;
+// int? y = 3;
+// int? z = x * y;
+// Console.WriteLine(z);
+
+ int? b = null;
+// int? b = 100;
+
+if (b.HasValue)
+{
+    Console.WriteLine($"b = {b.Value}");
+}
+else
+{
+    Console.WriteLine("b não possui um valor (null)");
+}
+
+Console.ReadKey();
--- CSFundamentos1/TiposDeDados4/obj/Debug/net6.0/TiposDeDados4.AssemblyInfo.cs
+++ CSFundamentos1/TiposDeDados4/obj/Debug/net6.0/TiposDeDados4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiposDeDados4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd3071122386e4743a6e923155c28936f66cd95c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd38cba30ca301542f7e6458f1d4b98a6200e85")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiposDeDados4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiposDeDados4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
