29677c7fdece5258fa17ee3e5aae80918a4c92f0
Tiago Ferrari <tiago-projects@outlook.com>
2024-03-04 13:35:25 -0300

    add OperadoresDeAtribuicao

--- CSFundamentos1/InferenciaTipos1/Program.cs
+++ CSFundamentos1/InferenciaTipos1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("## Inferência Tipos (var) ##");
+
+//int idade = 25;
+//string nome = "Maria";
+//decimal salario = 2500.00m;
+
+var idade = 25;
+var nome = "Maria";
+var salario = 2500.00m;
+
+Console.WriteLine($"{nome} tem {idade} anos e ganha {salario.ToString("c")}");
+
+// var limitações
+//var salario = null;
+//var titulo;
+//var imposto, total, salario;
+
+//// não posso mudar o tipo após inicializar
+//var num = 10;
+//num = num + 10;
+//num = "Teste";
+
+// funciona para tipos definidos pelo usuário
+var teste = new Teste();
+teste.MeuMetodo();
+
+
+Console.ReadKey();
+
+public class Teste
+{
+    public void MeuMetodo()
+    {
+        Console.WriteLine("Meu método");
+    }
+}
---
+++ CSFundamentos1/OperadorDeAtribuicao/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("## Operadores de Atribuição ##");
+
+// usando operadores  atribuição com tipos numéricos 
+
+//var x = 10;
+////x = 20;
+//Console.WriteLine($"Valor inicial de x = {x}");
+//Console.WriteLine($"x+=5 ==> {x += 5}");
+//Console.WriteLine($"x-=3 ==> {x -= 3}");
+//Console.WriteLine($"x*=4 ==> {x *= 4}");
+//Console.WriteLine($"x/=5 ==> {x /= 5}");
+//Console.WriteLine($"x%=5 ==> {x %= 5}");
+
+// usando operadores  atribuição com string
+var y = "123";
+//string y = null;
+Console.WriteLine($"O Valor inicial de y = {y}");
+Console.WriteLine($"y+=\"456\" ==> {y += "456"}");
+
+
+Console.ReadKey();
