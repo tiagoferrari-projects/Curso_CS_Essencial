f63cde5c31c998e2d9de11aedd62fe66f6e99de9
Tiago Ferrari <tiago-projects@outlook.com>
2024-03-03 23:34:07 -0300

    add InferenciaTipos1

--- CSFundamentos1/ExerciciosFase1Fundamentos/obj/Debug/net6.0/ExerciciosFase1Fundamentos.AssemblyInfo.cs
+++ CSFundamentos1/ExerciciosFase1Fundamentos/obj/Debug/net6.0/ExerciciosFase1Fundamentos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExerciciosFase1Fundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+846a3fab5a1db5e7fae6b9cb084e5088822e05e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f4940346dc607533f36c5226356341d978434f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExerciciosFase1Fundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExerciciosFase1Fundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CSFundamentos1/TipoDateTime1/obj/Debug/net6.0/TipoDateTime1.AssemblyInfo.cs
+++ CSFundamentos1/TipoDateTime1/obj/Debug/net6.0/TipoDateTime1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TipoDateTime1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+846a3fab5a1db5e7fae6b9cb084e5088822e05e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f4940346dc607533f36c5226356341d978434f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TipoDateTime1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TipoDateTime1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
