d4d5b3953cc5965545a6e9b43d9c1fcdc5e3d3c1
Tiago Ferrari <tiago-projects@outlook.com>
2024-03-08 01:31:37 -0300

    add  PrecedenciaOperadores1

---
+++ CSFundamentos1/PrecedenciaOperadores1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("## Precedência de Operadores ##\n");
+
+//int x = 10 - 2 * 3;
+//Console.WriteLine(x);
+
+//bool b = !(9 != 8) && 5 >= 7 || 8 >= 6;
+//bool b = !(9 != 8) && (5 >= 7 || 8 >= 6);
+//Console.WriteLine(b);
+
+//int a = 5, b = 6, c = 4;
+//int r = --a * b - ++c;
+//Console.WriteLine(r);
+
+
+// o valor de c (ou seja, 3)é atribuído a b, e então o valor de b é atribuído a a.
+// depois de executar esta instrução, os valores de a, b e c serão igual a 3.
+int a = 5, b = 6, c = 3;
+int resultado = a = b = c;
+Console.WriteLine(resultado);
+
+
+
+Console.ReadKey();
