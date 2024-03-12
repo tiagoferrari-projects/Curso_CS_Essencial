9b2f42dd7e115136004cf90e57e8241ef9e9b755
Tiago Ferrari <tiago-projects@outlook.com>
2024-03-12 02:01:47 -0300

    add 4Estrutura_Switch_Case parte 2

--- EstruturasDeControle/4Estrutura_Switch_Case/Program.cs
+++ EstruturasDeControle/4Estrutura_Switch_Case/Program.cs
@@ -25,17 +25,78 @@ switch (numeroParcelas)
 */
 
 // avaliando o valor de uma expressão: par ou impar
+//Console.WriteLine("Informe um número inteiro:\t");
+//var numero = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Informe um número inteiro:\t");
-var numero = Convert.ToInt32(Console.ReadLine());
+//switch(numero % 2)
+//{
+//    case 0:
+//        Console.WriteLine($"\n{numero} é PAR");
+//        break;
+//    case 1:
+//        Console.WriteLine($"\n{numero} é IMPAR");
+//        break;
+//}
 
-switch(numero % 2)
+// Aula: Instrução switch-case II
+
+//Console.WriteLine("Informe o nome do mês\t");
+//var mes = Console.ReadLine().ToLower();
+
+//switch (mes)
+//{
+//    case "janeiro":
+//    case "março":
+//    case "maio":
+//    case "julho":
+//    case "agosto":
+//    case "outubro":
+//    case "dezembro":
+//        Console.WriteLine("Este mês tem 31 dias");
+//        break;
+//    case "fevereiro":
+//        Console.WriteLine("Este mês tem 28 ou 29 dias");
+//        break;
+//    default:
+//        Console.WriteLine("Este mês tem 30 dias");
+//        break;
+//}
+
+// switch-case aninhado
+
+int cargo = 0;
+int funcao = 0;
+Console.WriteLine("Você é Gerente(1) ou Programador(2)\t");
+cargo = Convert.ToInt32(Console.ReadLine());
+
+if (cargo == 2)
 {
-    case 0:
-        Console.WriteLine($"\n{numero} é PAR");
-        break;
+    Console.WriteLine("Você é Junior(1) ou Senior(2)\t");
+    funcao = Convert.ToInt32(Console.ReadLine());
+}
+
+switch(cargo)
+{
     case 1:
-        Console.WriteLine($"\n{numero} é IMPAR");
+        Console.WriteLine("\nBem vindo Gerente");
+        break;
+    case 2:
+        Console.WriteLine("\nBem vindo Programador");
+        switch(funcao)
+        {
+            case 1:
+                Console.WriteLine("\nVocê é Junior");
+                break;
+            case 2:
+                Console.WriteLine("\nVocê é Senior");
+                break;
+            default:
+                Console.WriteLine("\nFunção desconhecida");
+                break;
+        }
+        break;
+    default:
+        Console.WriteLine("Não consigo te identificar");
         break;
 }
 
--- EstruturasDeControle/4Estrutura_Switch_Case/obj/Debug/net6.0/4Estrutura_Switch_Case.AssemblyInfo.cs
+++ EstruturasDeControle/4Estrutura_Switch_Case/obj/Debug/net6.0/4Estrutura_Switch_Case.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4Estrutura_Switch_Case")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52ea224b1cea2daf8b3693521cd503c26689867e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c22127d2671c2271a5e91477d6145388473b0e3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("4Estrutura_Switch_Case")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4Estrutura_Switch_Case")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
