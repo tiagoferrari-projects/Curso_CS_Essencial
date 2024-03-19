bdc3fc7e27d5c4a115dce8f2fe09b2c0611b8897
Tiago Ferrari <tiago-projects@outlook.com>
2024-03-18 21:07:26 -0300

    add resolucao Exercicio_09

--- EstruturasDeControle/Exercicio_09/Program.cs
+++ EstruturasDeControle/Exercicio_09/Program.cs
@@ -1,5 +1,19 @@
 ﻿Console.WriteLine("-- EXERCICIO 09 --\n");
 
+Console.WriteLine("9- Escreva um programa para exibir as tabelas de multiplicação do 2 ao 6 usando o loop do-while aninhado\n");
 
-Console.WriteLine("\n\nFim do processamento...");
+int numero = 2;
+do
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        int resultado = numero * i;
+        Console.WriteLine($"{numero} x {i} = {resultado}");
+    }
+    Console.WriteLine();
+    numero++;
+} while (numero <= 6);
+
+
+Console.WriteLine("\nFim do processamento...");
 Console.ReadKey();
--- EstruturasDeControle/Exercicio_09/obj/Debug/net6.0/Exercicio_09.AssemblyInfo.cs
+++ EstruturasDeControle/Exercicio_09/obj/Debug/net6.0/Exercicio_09.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio_09")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb1695a927752fff597750886ef23fea5c445f66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5541801b55e6729f64beb3c0aa71084716f032ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio_09")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio_09")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
