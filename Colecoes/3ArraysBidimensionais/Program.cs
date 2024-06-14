415f4151a3783960167ca822b9fcabbb6f140be7
tiagoferrari-sectra <tiago-projects@outlook.com>
2024-06-14 14:16:19 -0300

    add 3ArraysBidimensionais

---
+++ Colecoes/3ArraysBidimensionais/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("## Arrays Bidimensionais\n");
+
+
+Console.WriteLine("\nArray a");
+int[,] a;
+
+a = new int[2, 2];
+
+a[0, 0] = 0;
+a[0, 1] = 1;
+a[1, 0] = 2;
+a[1, 1] = 3;
+
+Console.WriteLine(a[0, 0]);
+Console.WriteLine(a[0, 1]);
+Console.WriteLine(a[1, 0]);
+Console.WriteLine(a[1, 1]);
+
+Console.WriteLine("\nArray b");
+int[,] b = new int[2, 2] { { 10, 20 }, { 30, 40 } };
+Console.WriteLine(b[0, 0]);
+Console.WriteLine(b[0, 1]);
+Console.WriteLine(b[1, 0]);
+Console.WriteLine(b[1, 1]);
+
+
+Console.ReadKey();
