a8e0ffed424688b03ae09a041df66c2cc7ad24ec
tiagoferrari-sectra <tiago-projects@outlook.com>
2024-07-04 00:39:54 -0300

    add 11Indexadores

---
+++ Colecoes/11Indexadores/Program.cs
@@ -0,0 +1,32 @@
+﻿using _11Indexadores;
+
+Console.WriteLine("## Indexadores ##\n");
+
+Time time = new Time();
+
+// atribui valores
+time[1] = "Corinthians";
+time[3] = "Real Madrid";
+time[5] = "Manchester City";
+time[7] = "Liverpool";
+time[-1] = "Milan";
+time[1000] = "Chelsea";
+
+// obtendo valores
+string valor1 = time[1];
+string valor2 = time[3];
+string valor3 = time[5];
+string valor4 = time[7];
+string valor5 = time[-1];
+string valor6 = time[1000];
+
+// exibir
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);    
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+Console.WriteLine(valor6);
+
+
+Console.ReadKey();
---
+++ Colecoes/11Indexadores/Time.cs
@@ -0,0 +1,29 @@
+﻿namespace _11Indexadores
+{
+    public class Time
+    {
+        //definir os limites do array
+        string[] valor = new string[10];
+
+        public string this[int i]
+        {
+            get
+            {
+                if (i >= 0 && i < valor.Length)
+                {
+                    // retorna o valor 
+                    return valor[i];
+                }
+                return "Erro";
+            }
+            set
+            {
+                if (i >= 0 && i < valor.Length)
+                {
+                    // atribuir o valor
+                    valor[i] = value;
+                }
+            }
+        }
+    }
+}
