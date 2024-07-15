b81c98f214d9035a8f2db92123a2786f15f444ca
tiagoferrari-sectra <tiago-projects@outlook.com>
2024-07-14 23:54:32 -0300

    add 5Heranca_Modificador_New

--- ProgramacaoOrientadaObjetos/1Heranca_ConceitoSintaxe/obj/Debug/net7.0/1Heranca_ConceitoSintaxe.AssemblyInfo.cs
+++ ProgramacaoOrientadaObjetos/1Heranca_ConceitoSintaxe/obj/Debug/net7.0/1Heranca_ConceitoSintaxe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1Heranca_ConceitoSintaxe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d092dc590bbc14c8693b6360742f915ba51c4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a506cd8a77d8e01d28f35d9a21d0d52d8e7d2115")]
 [assembly: System.Reflection.AssemblyProductAttribute("1Heranca_ConceitoSintaxe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1Heranca_ConceitoSintaxe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
